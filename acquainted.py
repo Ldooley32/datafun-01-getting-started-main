bdf6d6e89b1bc61fe644bb9dc28d352cee6de2dd
ldooley32 <s567892@nwmissouri.edu>
2023-09-06 23:06:51 -0500

    add report

---
+++ acquainted.py
@@ -0,0 +1,84 @@
+"""
+Purpose: Get acquainted with basic Python concepts.
+
+Author: Denise Case
+
+This file name is:   acquainted.py
+This module name is: acquainted
+
+Statements:
+- All Python programs are composed of statements.
+- Statements are instructions that tell the computer to do something.
+- Each statement is written on a separate line.
+
+Variables:
+- Variables are used to store values.
+- Variables are created when they are first assigned a value.
+- Variables are used to refer to values later in the program.
+- Variables can be reassigned to refer to a different value.
+
+Comments:
+- Comments are used to document code.
+- Comments are ignored by the Python interpreter.
+- Comments are preceded by the # symbol.
+- Comments can be placed on their own line or at the end of a line.
+- Comments can be used to disable code.
+
+Opening Remarks:
+- These introductory remarks are comments. 
+- By wrapping a comment in triple quotes, we can make multi-line comments.
+"""
+
+# ----------------- INSTRUCTOR GENERATED CODE -----------------
+
+# Use this handy logger to document your work automatically
+
+# import setup_logger function from instructor-generated module
+from util_logger import setup_logger
+
+# setup the logger using the current file name (a built-in variable)
+logger, logname = setup_logger(__file__)
+
+# ----------------- END INSTRUCTOR GENERATED CODE -----------------
+
+
+# Declare some variables - Python will infer the data type
+# Try string, integer, float, and boolean (True/False) variables
+
+# TODO: Customize these by changing the values - use your own name, etc.
+
+# String data types
+name = "Laura Dooley"
+state = "Nebraska"
+country = "USA"
+
+# Integer data types
+pet_count = 2
+skill_count = 0
+
+# Float data types (floating point numbers, with a decimal point)
+temperature_f = 100.5
+pet_weight_lbs = 12.4
+
+
+# Boolean data types (True or False)
+has_dog = False
+likes_analytics = True
+likes_python = True
+
+# Log some information using f-strings (formatted strings)
+# f-strings are a convenient way to embed variables and expressions in strings
+# the f is placed BEFORE the opening quote
+# and variables (or expressions) are placed inside curly braces
+
+logger.info(f"Name: {name} ")
+logger.info(f"State: {state}")
+logger.info(f"Country: {country}")
+logger.info(f"Pet count: {pet_count}")
+logger.info(f"Double the pet count: {pet_count * 2}")
+logger.info(f"Skill count: {skill_count}")
+logger.info(f"Temperature: {temperature_f}")
+logger.info(f"Pet weight: {pet_weight_lbs}")
+logger.info(f"Has dog: {has_dog}")
+logger.info(f"Likes analytics: {likes_analytics}")
+logger.info(f"Likes Python: {likes_python}")
---
+++ basic_expressions.py
@@ -0,0 +1,74 @@
+"""
+Purpose: Illustrate basic expressions and operators in Python.
+
+Author: Denise Case
+
+This file name is:   basic_expressions.py
+This module name is: basic_expressions
+
+Expressions
+
+Data analytics is all about getting value out of data.
+Expressions are the building blocks of data analytics.
+
+Rather like math expressions, or Excel expressions, Python expressions
+are a combination of values, variables, operators, and function calls.
+
+Expressions are made of operands and operators.
+
+- Operators are symbols like +, -, *, /, and =.
+- Operands can be values or variables, 
+  and can include function calls like len() and str().
+
+Writing expressions in Python is like writing formulas in Excel.
+ 
+"""
+
+# ----------------- INSTRUCTOR GENERATED CODE -----------------
+
+# Use this handy logger to document your work automatically
+
+# import setup_logger function from instructor-generated module
+from util_logger import setup_logger
+
+# setup the logger using the current file name (a built-in variable)
+logger, logname = setup_logger(__file__)
+
+# ----------------- END INSTRUCTOR GENERATED CODE -----------------
+
+# Declare  Variables
+# TODO: Try changing the values of these variables
+# TODO: Add some new variables (like rectangle_length and rectangle_width)
+#       and calculate the area of a rectangle (rectangle_area = rectangle_length * rectangle_width)
+triangle_base = 13
+triangle_height = 7
+num1 = 560
+num2 = 25
+float_num1 = 15.4
+float_num2 = 26.8
+float_num3 = 35.9
+
+#Rectangle Variables
+rectangle_length = 45
+rectangle_width = 8
+
+# Basic Arithmetic Operations
+triangle_area = triangle_base * triangle_height / 2
+total_sum = float_num1 + float_num2
+difference = num1 - num2
+
+#Rectangle Area
+rectangle_area = rectangle_length * rectangle_width
+
+# Log Information
+logger.info(
+    f"Given base={triangle_base} and height={triangle_height}, triangle area = {triangle_area}"
+)
+logger.info(
+    f"Given float_num1={float_num1} and float_num2={float_num2}, sum = {total_sum}"
+)
+logger.info(f"Given num1={num1} and num2={num2}, the difference = {difference}"
+)
+logger.info(f"Rectangle length = {rectangle_length}, and rectangle_width = {rectangle_width}, rectangle_area = {rectangle_area}" 
+)
+          
